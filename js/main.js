591fd51d3e6c8975f7ceceab72eb041addba70cc
Zearw <sofiacorzo1996@gmail.com>
2023-12-07 09:17:43 -0300

    Agrego funcionalidad a la seccion de emociones

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const btnFeliz = document.getElementById('btn-cara_feliz')
+const btnTriste = document.getElementById('btn-cara-triste')
+const btnPreocupado = document.getElementById('btn-cara-preocupada')
+const modalEmocionesContainer = document.getElementById('modal-emociones-container')
+const modalEmociones = document.querySelector('.modal')
+const modalEmocionesTexto = document.getElementById('modal-emociones-texto')
+
+const cerrarModalEmociones = document.getElementById('cerrar-modal-emociones')
+
+
+async function getFrases() {
+  const frasesURL = '../json/frases.json'
+  const res = await fetch(frasesURL)
+  const data = await res.json()
+  return data
+}
+
+const escojerEmocion = async (emocion) => {
+
+  const data = await getFrases()
+  let indicesEmocion = []
+  
+  for( let i = 0; i < data.length; i++) {
+    if(data[i].emocion === emocion) {
+      indicesEmocion.push(data[i])
+    }
+  }
+
+  let auxIndice = Math.floor(Math.random() * indicesEmocion.length)
+
+
+
+  const textoModal = document.createElement('p')
+  const  auxtextoModal = document.createTextNode(indicesEmocion[auxIndice].texto)
+  textoModal.setAttribute('id', 'frase-motivacional')
+  textoModal.appendChild(auxtextoModal)
+
+  modalEmocionesTexto.append(textoModal)
+}
+
+
+btnFeliz.addEventListener('click', (e) => {
+  e.preventDefault()
+  modalEmocionesContainer.style.opacity = "1"
+  modalEmocionesContainer.style.visibility = "visible"
+  modalEmociones.classList.toggle("modal-close")
+  escojerEmocion('feliz')
+  confetti()
+})
+
+btnTriste.addEventListener('click', (e) => {
+  e.preventDefault()
+  modalEmocionesContainer.style.opacity = "1"
+  modalEmocionesContainer.style.visibility = "visible"
+  modalEmociones.classList.toggle("modal-close")
+  escojerEmocion('triste')
+})
+
+btnPreocupado.addEventListener('click', (e) => {
+  e.preventDefault()
+  modalEmocionesContainer.style.opacity = "1"
+  modalEmocionesContainer.style.visibility = "visible"
+  modalEmociones.classList.toggle("modal-close")
+  escojerEmocion('preocupada')
+})
+
+
+cerrarModalEmociones.addEventListener('click', ()=> {
+  let frase = document.getElementById('frase-motivacional')
+  modalEmociones.classList.toggle("modal-close")
+
+  setTimeout(()=> {
+    modalEmocionesContainer.style.opacity = "0"
+    modalEmocionesContainer.style.visibility = "hidden"
+  modalEmocionesTexto.removeChild(frase)
+  }, 700)
+  
+  
+})
